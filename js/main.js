008c8b656c4db3b538505ed3734a7e4dae11b499
Hugo Mendes <hugo-vmendes@hotmail.com>
2020-09-29 15:30:16 -0300

    Alterando parametros da função

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 // Código Javascript
 
+
+var mult = false;
+var soma = false;
+var div = false;
+var sub = false;
+var numero1;
+var numero2;
+
+
 function pegaInput(idOpcao){
 	var op = document.getElementById(idOpcao).value;
 
@@ -8,146 +17,173 @@ function pegaInput(idOpcao){
 	}
 
 	if(op == '/' && document.getElementById("resultado-mostra").value != 0){
-		document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +' '+op;
+
+		document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +op;
 	}
 
 	if(op == '*'){
-		document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +' '+op;
+	
+		document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +' '+op+' ';
 	}
 
 	if(op == '+'){
-		document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +' '+op;
+		soma = true;
+
+		document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +' '+op+' ';
 	}
 
 	if(op == '.'){
 		document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +''+op;
 	}
 
+	if(op == '-'){
+		document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +''+op;
+	}
+
 	if(op == '='){
-		
-	}
 
-	if(op == '0'){
-		document.getElementById("resultado-mostra").value = op;
-	}
-
-	if(op == '1'){
-
-		if(document.getElementById("resultado-mostra").value == 0){
-			document.getElementById("resultado-mostra").value = op;
+		if(parseFloat(document.getElementById("resultado-mostra").value) == 0){
+			document.getElementById("resultado-mostra").value = 0;
 		}
 
-		else{
-			document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +' '+op;
-		}
-		
-	}
 
-
-	if(op == '2'){
-
-		if(document.getElementById("resultado-mostra").value == 0){
-			document.getElementById("resultado-mostra").value = op;
-		}
-
-		else{
-			document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +' '+op;
-		}
-		
-	}
-
-
-	if(op == '3'){
-
-		if(document.getElementById("resultado-mostra").value == 0){
-			document.getElementById("resultado-mostra").value = op;
-		}
-
-		else{
-			document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +' '+op;
-		}
-		
-	}
-
-
-	if(op == '4'){
-
-		if(document.getElementById("resultado-mostra").value == 0){
-			document.getElementById("resultado-mostra").value = op;
-		}
-
-		else{
-			document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +' '+op;
-		}
-		
-	}
-
-
-
-	if(op == '5'){
-
-		if(document.getElementById("resultado-mostra").value == 0){
-			document.getElementById("resultado-mostra").value = op;
-		}
-
-		else{
-			document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +' '+op;
-		}
-		
-	}
-
-
-	if(op == '6'){
-
-		if(document.getElementById("resultado-mostra").value == 0){
-			document.getElementById("resultado-mostra").value = op;
-		}
-
-		else{
-			document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +' '+op;
-		}
-		
-	}
-
-
-	if(op == '7'){
-
-		if(document.getElementById("resultado-mostra").value == 0){
-			document.getElementById("resultado-mostra").value = op;
-		}
-
-		else{
-			document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +' '+op;
-		}
-		
-	}
-
-
-	if(op == '8'){
-
-		if(document.getElementById("resultado-mostra").value == 0){
-			document.getElementById("resultado-mostra").value = op;
-		}
-
-		else{
-			document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +' '+op;
-		}
-		
-	}
-
-
-	if(op == '9'){
-
-		if(document.getElementById("resultado-mostra").value == 0){
-			document.getElementById("resultado-mostra").value = op;
-		}
-
-		else{
-			document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +' '+op;
-		}
-		
 	}
 
 	
+}
+
+function pegaInputNum(idOpcao){
+	var num = document.getElementById(idOpcao).value;
+	if(num == '0'){
+
+		if(parseFloat(document.getElementById("resultado-mostra").value) == 0){
+		
+			document.getElementById("resultado-mostra").value = num;
+		}
+
+		else{
+			document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value + op;
+		}
+		
+	}
+
+	if(num == '1'){
+
+		if(document.getElementById("resultado-mostra").value == 0){
+			document.getElementById("resultado-mostra").value = num;
+		}
+
+		else{
+			numero1 = parseFloat(num);
+			document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +num;
+		}
+		
+	}
+
+
+	if(num == '2'){
+
+		if(document.getElementById("resultado-mostra").value == 0){
+			document.getElementById("resultado-mostra").value = num;
+		}
+
+		else{
+			numero2 = parseFloat(num);
+			document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +num;
+		}
+		
+	}
+
+
+	if(num == '3'){
+
+		if(document.getElementById("resultado-mostra").value == 0){
+			document.getElementById("resultado-mostra").value = num;
+		}
+
+		else{
+			document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +num;
+		}
+		
+	}
+
+
+	if(num == '4'){
+
+		if(document.getElementById("resultado-mostra").value == 0){
+			document.getElementById("resultado-mostra").value = num;
+		}
+
+		else{
+			document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +num;
+		}
+		
+	}
+
+
+
+	if(num == '5'){
+
+		if(document.getElementById("resultado-mostra").value == 0){
+			document.getElementById("resultado-mostra").value = num;
+		}
+
+		else{
+			document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +num;
+		}
+		
+	}
+
+
+	if(num == '6'){
+
+		if(document.getElementById("resultado-mostra").value == 0){
+			document.getElementById("resultado-mostra").value = num;
+		}
+
+		else{
+			document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +num;
+		}
+		
+	}
+
+
+	if(num == '7'){
+
+		if(document.getElementById("resultado-mostra").value == 0){
+			document.getElementById("resultado-mostra").value = num;
+		}
+
+		else{
+			document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +num;
+		}
+		
+	}
+
+
+	if(num == '8'){
+
+		if(document.getElementById("resultado-mostra").value == 0){
+			document.getElementById("resultado-mostra").value = num;
+		}
+
+		else{
+			document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +num;
+		}
+		
+	}
+
+
+	if(num == '9'){
+
+		if(document.getElementById("resultado-mostra").value == 0){
+			document.getElementById("resultado-mostra").value = num;
+		}
+
+		else{
+			document.getElementById("resultado-mostra").value = document.getElementById("resultado-mostra").value +num;
+		}
+		
+	}
 
 }
